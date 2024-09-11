e8853aca2bb9e8c8b076523c67d6e167113ec3ab
gianlucaromeo <gianlucaromeo@outlook.com>
2024-09-11 09:40:59 +0300

    Exercise 1.6-1.11 - Unicafe, Steps 3, 4, 5

--- Part 1 - Introduction to React/1.6-1.11 - Unicafe/src/App.jsx
+++ Part 1 - Introduction to React/1.6-1.11 - Unicafe/src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+}
+
+const StatisticsLine = (props) => (
+  <p>
+    {props.text} {props.value}
+  </p>
+)
+
+StatisticsLine.propTypes = {
+  text: PropTypes.string.isRequired,
+  value: PropTypes.number,
+}
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
 
   const total = good + neutral + bad
   const average = (good - bad) / total
@@ -11,17 +32,41 @@ const App = () => {
 
   return (
     <>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average || 0}</p>
-      <p>positive {positive || 0}%</p>
+      {
+        total === 0
+          ? <p>No feedback given</p>
+          : <>
+              <StatisticsLine text="good" value={good} />
+              <StatisticsLine text="neutral" value={neutral} />
+              <StatisticsLine text="bad" value={bad} />
+              <StatisticsLine text="all" value={total} />
+              <StatisticsLine text="average" value={average || 0} />
+              <StatisticsLine text="positive" value={positive || 0} />
+            </>
+      }
+    </>
+  )
+}
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
