a7269f15b09015943ba5cdd108afdf4124076347
gianlucaromeo <gianlucaromeo@outlook.com>
2024-09-20 10:23:08 +0300

    Added local server and db.json to Phonebook

--- Part 2 - Communicating with server/2.6-2.17 - Phonebook/src/App.jsx
+++ Part 2 - Communicating with server/2.6-2.17 - Phonebook/src/App.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import { useEffect } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  // TODO: Add a new person to the db.json file
   const addPerson = (event) => {
     event.preventDefault()
 
@@ -50,12 +56,12 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h3>add a new</h3>
-      <PersonForm 
-        newName={newName} 
-        handleNameChange={handleNameChange} 
+      <PersonForm
+        newName={newName}
+        handleNameChange={handleNameChange}
         newNumber={newNumber}
         handleNumberChange={handleNumberChange}
-        addPerson={addPerson}  
+        addPerson={addPerson}
       />
       <h3>Numbers</h3>
       <Persons persons={filteredPersons} />
