3df2ab452ba802f14d1d2e4348402e1b53fc1bc1
gianlucaromeo <gianlucaromeo@outlook.com>
2024-09-21 18:11:43 +0300

    Exercise 3.1-3.8 - Phonebook backend - Added unknownEndpoint middleware

--- Part 3 - Programming a server with NodeJS and Express/3.1-3.8 Phonebook backend/index.js
+++ Part 3 - Programming a server with NodeJS and Express/3.1-3.8 Phonebook backend/index.js
@@ -91,3 +91,9 @@ const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
