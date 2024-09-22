5a59afa0fbf2ce766bf0af360f6ae51399a33432
gianlucaromeo <gianlucaromeo@outlook.com>
2024-09-22 20:11:19 +0300

    uibuild

--- Part 3 - Programming a server with NodeJS and Express/3.1-3.8 Phonebook backend/index.js
+++ Part 3 - Programming a server with NodeJS and Express/3.1-3.8 Phonebook backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 var morgan = require('morgan')
 app.use(
