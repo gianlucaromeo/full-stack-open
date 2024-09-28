6a1c02e056d0ac0b849bc748948e7fec7bfd5159
gianlucaromeo <gianlucaromeo@outlook.com>
2024-09-28 16:17:59 +0300

    uibuild

---
+++ Part 3 - Programming a server with NodeJS and Express/3.13-3.18 Phonebook backend/index.js
@@ -0,0 +1,83 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
+
+const Person = require('./models/person')
+
+var morgan = require('morgan')
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'),
+            '-',
+            tokens['response-time'](req, res), 'ms',
+            JSON.stringify(req.body)
+        ].join(' ')
+    })
+)
+
+app.get('/api/persons', (req, res) => {
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    Person.findById(req.params.id).then(person => {
+        res.json(person)
+    })
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    // FIX: If person already exists, it currently creates a new person with 
+    // the same name and number
+
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    // TODO: Implement method to delete person
+    res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+    })
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
