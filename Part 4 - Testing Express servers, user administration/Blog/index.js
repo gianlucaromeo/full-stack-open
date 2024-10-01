35859141c5026bfa2cd8ab7187b30fcf16ae295f
gianlucaromeo <gianlucaromeo@outlook.com>
2024-10-01 22:22:39 +0300

    Exercise 4.2 - Blog List, Step 2

--- Part 4 - Testing Express servers, user administration/Blog/index.js
+++ Part 4 - Testing Express servers, user administration/Blog/index.js
@@ -1,36 +1,7 @@
-require('dotenv').config()
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const express = require('express')
-const cors = require('cors')
-
-const app = express()
-
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-
-const Blog = require('./models/blog')
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  console.log('request.body:', request.body)
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
