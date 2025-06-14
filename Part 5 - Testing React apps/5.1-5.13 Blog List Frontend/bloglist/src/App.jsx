a4948bc6c19430e733b7ffd6114d41595571898c
gianlucaromeo <gianlucaromeo@outlook.com>
2025-06-14 18:43:13 +0300

    Exercise 5.10 - Blog List Frontend, Step 10

--- Part 5 - Testing React apps/5.1-5.13 Blog List Frontend/bloglist/src/App.jsx
+++ Part 5 - Testing React apps/5.1-5.13 Blog List Frontend/bloglist/src/App.jsx
@@ -134,7 +134,7 @@ const App = () => {
       {createBlogForm()}
 
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
     </div>
