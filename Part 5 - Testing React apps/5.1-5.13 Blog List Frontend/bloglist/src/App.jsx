b96e03efb543b6fc8e47cf539f0566f301ec3f15
gianlucaromeo <gianlucaromeo@outlook.com>
2025-06-14 16:28:37 +0300

    Exercise 5.4 - Blog List Frontend, Step 3, 4, 5

--- Part 5 - Testing React apps/5.1-5.13 Blog List Frontend/bloglist/src/App.jsx
+++ Part 5 - Testing React apps/5.1-5.13 Blog List Frontend/bloglist/src/App.jsx
@@ -2,56 +2,136 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import InfoMessage from './components/InfoMessage'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState('')
 
-  useEffect(() => {
+
+  const fetchBlogs = async () => {
+    const blogs = await blogService.getAll()
+    setBlogs(blogs)
+  }
+
+  const checkIfUserIsLoggedIn = () => {
     const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
       blogService.setToken(user.token)
     }
+  }
+
+  useEffect(() => {
+    checkIfUserIsLoggedIn()
   }, [])
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    fetchBlogs()
   }, [])
+
+  const handleMessage = (message, type) => {
+    setMessage(message)
+    setMessageType(type)
+    setTimeout(() => {
+      setMessage(null)
+      setMessageType('')
+    }, 5000)
+  }
+
+  const handleResetUser = () => {
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
-    
+
     try {
       const user = await loginService.login({
         username, password,
       })
       window.localStorage.setItem(
         'loggedBloglistUser', JSON.stringify(user)
-      ) 
-      setUser(user)
-      setUsername('')
-      setPassword('')
+      )
+      checkIfUserIsLoggedIn()
+      handleMessage('welcome back', 'success')
+      fetchBlogs()
     } catch (exception) {
-      console.log('wrong credentials', exception)
+      console.log(exception)
+      handleMessage('wrong credentials', 'error')
     }
   }
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBloglistUser')
-    setUser(null)
+    handleResetUser()
   }
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url,
+    }
+    try {
+      await blogService.create(blogObject)
+      handleMessage('blog created successfully', 'success')
+      fetchBlogs()
+    } catch (exception) {
+      console.log(exception)
+      handleMessage('failed to create blog', 'error')
+    }
+  }
+
+  const createBlogForm = () => (
+    <form onSubmit={handleCreateBlog}>
+      <div>
+        title
+        <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author
+        <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url
+        <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
         username
-          <input
+        <input
           type="text"
           value={username}
           name="Username"
@@ -60,7 +140,7 @@ const App = () => {
       </div>
       <div>
         password
-          <input
+        <input
           type="password"
           value={password}
           name="Password"
@@ -68,12 +148,13 @@ const App = () => {
         />
       </div>
       <button type="submit">login</button>
-    </form>      
+    </form>
   )
 
   if (user === null) {
     return (
       <div>
+        <InfoMessage message={message} type={messageType} />
         <h2>Log in to application</h2>
         {loginForm()}
       </div>
@@ -82,9 +163,16 @@ const App = () => {
 
   return (
     <div>
+      <InfoMessage message={message} type={messageType} />
+
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
       <button onClick={handleLogout}>logout</button>
+
+      <h2>create new</h2>
+      {createBlogForm()}
+
+      <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ Part 5 - Testing React apps/5.1-5.13 Blog List Frontend/bloglist/src/components/InfoMessage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InfoMessage = ({ message, type }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={type}>
+      {message}
+    </div>
+  )
+}
+
+export default InfoMessage  
