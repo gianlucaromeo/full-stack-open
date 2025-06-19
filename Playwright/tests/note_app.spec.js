d559e639af9d24907c131df7690d475dcf1cf143
gianlucaromeo <gianlucaromeo@outlook.com>
2025-06-19 16:40:37 +0300

    Added Playwright tests

---
+++ Playwright/tests/note_app.spec.js
@@ -0,0 +1,38 @@
+const { test, expect } = require('@playwright/test')
+
+test.describe('Note app', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173')
+    })
+
+    test('front page can be opened', async ({ page }) => {
+        const locator = await page.getByText('Notes')
+        await expect(locator).toBeVisible()
+        await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2023')).toBeVisible()
+    })
+
+    test('login form can be opened', async ({ page }) => {
+        await page.getByRole('button', { name: 'login' }).click()
+        await page.getByTestId('username').fill('username3')
+        await page.getByTestId('password').fill('password3')
+
+        await page.getByRole('button', { name: 'login' }).click()
+        await expect(page.getByText('name1 logged in')).toBeVisible()
+    })
+
+    test.describe('when logged in', () => {
+        test.beforeEach(async ({ page }) => {
+            await page.getByRole('button', { name: 'log in' }).click()
+            await page.getByTestId('username').fill('username3')
+            await page.getByTestId('password').fill('password3')
+            await page.getByRole('button', { name: 'login' }).click()
+        })
+
+        test('a new note can be created', async ({ page }) => {
+            await page.getByRole('button', { name: 'new note' }).click()
+            await page.getByRole('textbox').fill('a note created by playwright')
+            await page.getByRole('button', { name: 'save' }).click()
+            await expect(page.getByText('a note created by playwright')).toBeVisible()
+        })
+    })
+})
